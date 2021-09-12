49a932789f14efc182ba3d8330b52de8030c0fa0
unknown <catrosits@gmail.comgit config --global user.name>
2021-09-13 07:56:56 +0800

    initial pages plus bootstrapping

---
+++ pages/about.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+	return (
+		<div>
+			<h1>Hello, this is about</h1>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente
+				dolores odit totam. Blanditiis non est expedita suscipit impedit ipsum,
+				iusto similique amet nesciunt quas. Soluta inventore consequatur
+				deleniti eos hic!
+			</p>
+		</div>
+	);
+};
+
+export default About;
